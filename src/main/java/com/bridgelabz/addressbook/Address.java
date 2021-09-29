37f52e1f27df63a742327dc1bcf6de3d4f7e2a3d
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-30 02:51:02 +0530

    ADDED: POJO class added to implement the er database model designed

---
+++ src/main/java/com/bridgelabz/addressbook/Address.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.addressbook;
+
+public class Address {
+
+    String address;
+    String city;
+    String state;
+    long zip;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public long getZip() {
+        return zip;
+    }
+
+    public void setZip(long zip) {
+        this.zip = zip;
+    }
+}
---
+++ src/main/java/com/bridgelabz/addressbook/ContactType.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbook;
+
+import java.util.List;
+
+public class ContactType {
+
+    String type_name;
+    List<Contact> contactList;
+}
