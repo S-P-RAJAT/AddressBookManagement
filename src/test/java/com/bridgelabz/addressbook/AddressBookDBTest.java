ba0b9388ecfd25c479a646ab503a668d19d8a16c
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-10-01 02:42:26 +0530

    ADDED: Test case to validate the update operation with the database

--- src/test/java/com/bridgelabz/addressbook/AddressBookDBTest.java
+++ src/test/java/com/bridgelabz/addressbook/AddressBookDBTest.java
@@ -1,9 +1,13 @@
 package com.bridgelabz.addressbook;
 
 
+import java.util.HashMap;
 import java.util.List;
 import java.sql.SQLException;
+
+import org.junit.Assert;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 import static com.bridgelabz.addressbook.AddressBookIOService.readData;
@@ -15,5 +19,21 @@ public class AddressBookDBTest {
     public void givenAddressBookDB_WhenRetrieved_ShouldMatchContactCount() {
         List<AddressBook> addressBookList = readData(IOService.DB_IO);
         assertEquals(2, addressBookList.size());
+    }
+
+    @Test
+    public void givenUpdateQuery_WhenSuccessful_ShouldSyncWithDB() throws SQLException {
+        AddressBookImpl addressBookService = new AddressBookImpl();
+        AddressBookDBService addressBookDBService = AddressBookDBService.getInstance();
+        List<AddressBook> addressBookList = addressBookDBService.readContactsFromDatabase();
+        addressBookService.setAddressBook(addressBookList.get(0));
+        String firstName = "Rajat";
+        String lastName = "S P";
+        Contact contact = addressBookService.getContact(firstName, lastName);
+        addressBookService.updateCity(firstName, lastName, contact, "Bangalore");
+        addressBookList = addressBookDBService.readContactsFromDatabase();
+        Assert.assertEquals(contact, addressBookList.get(0).contactList.get("Rajat" + " " + "S P"));
+
+
     }
 }
